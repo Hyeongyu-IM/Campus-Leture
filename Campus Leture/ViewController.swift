7383eda975ec109e4c6fff3af7505ee8c6b0eb20
Hyeongyu-IM <64323969+Hyeongyu-IM@users.noreply.github.com>
2020-09-19 16:11:44 +0900

    Initial Commit

---
+++ Campus Leture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Campus Leture
+//
+//  Created by 임현규 on 2020/09/19.
+//  Copyright © 2020 임현규. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Campus LetureTests/Campus_LetureTests.swift
@@ -0,0 +1,34 @@
+//
+//  Campus_LetureTests.swift
+//  Campus LetureTests
+//
+//  Created by 임현규 on 2020/09/19.
+//  Copyright © 2020 임현규. All rights reserved.
+//
+
+import XCTest
+@testable import Campus_Leture
+
+class Campus_LetureTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
