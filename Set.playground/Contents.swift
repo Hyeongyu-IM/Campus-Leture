7268262724cdc0c5a6e7dccb71857ab7b816f348
Hyeongyu-IM <64323969+Hyeongyu-IM@users.noreply.github.com>
2020-09-22 22:03:40 +0900

    Set

---
+++ Set.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+
+var someArray: Array<Int> = [1, 2, 3, 1]
+var someSet: Set<Int> = [1, 2, 3, 1]      // 중복되는 값을 넣어도 접근할수 없다
+
+
+someSet.isEmpty
+someSet.count
+
+someSet.contains(4)
+someSet.contains(1)
+
+someSet.insert(5)
+someSet
+
+someSet.remove(1)
+someSet
+
+
+
